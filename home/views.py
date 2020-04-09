84365b6e8ce2d0dd9ecc2ed9c84c9ddadd1ab653
siskin21 <siskin@isloco.com>
2020-04-09 21:47:02 +0900

    프로젝트 생성

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request):
+    # return HttpResponse("<p>Hello World!</p>")
+    return render(request, "index.html", {}) #render() :request, index.html을 조합하여 HttpResponse를 반환
